29152fe3317089bfd01970b99501828621879005
ZiHao257 <163956679+ZiHao257@users.noreply.github.com>
2024-03-19 01:36:16 -0400

    Update CRUD.py

--- CRUD.py
+++ CRUD.py
@@ -1,7 +1,15 @@
 import psycopg2
 
+# Global variable for the database connection
+connection = None
+
+# Connect to the database
+def connect_to_database(database, username, password):
+    global connection
+    connection = psycopg2.connect(dbname=database, user=username, password=password)
+
 # Gets all the student from the table
-def getAllStudents(connection):
+def getAllStudents():
     try:
         query = "SELECT * FROM students"
         with connection.cursor() as cursor:
@@ -18,7 +26,7 @@ def getAllStudents(connection):
         print("\nError retrieving the students from the database: \n", e)
 
 # Create addStudent function
-def addStudent(connection, first_name, last_name, email, enrollment_date):
+def addStudent(first_name, last_name, email, enrollment_date):
     try:
         query = "INSERT INTO students (first_name, last_name, email, enrollment_date) VALUES (%s, %s, %s, %s)"
         values = (first_name, last_name, email, enrollment_date)
@@ -32,7 +40,7 @@ def addStudent(connection, first_name, last_name, email, enrollment_date):
         print("\nError adding the student to the database:", e)
 
 # Update the student's email
-def updateStudentEmail(connection, student_id, new_email):
+def updateStudentEmail(student_id, new_email):
     try:
         query = "UPDATE students SET email = %s WHERE student_id = %s"
         values = (new_email, student_id)
@@ -48,64 +56,64 @@ def updateStudentEmail(connection, student_id, new_email):
         print("\nError updating the student's email\n", e)
 
 # Deleting a student from the database 
-def deleteStudent(connection, student_id):
+def deleteStudent(student_id):
     try:
         query = "DELETE FROM students WHERE student_id = %s"
-        values = (student_id)
+        values = (student_id,)
         with connection.cursor() as cursor:
-            cursor.execute(query, (values))
+            cursor.execute(query, values)
+            rows_affected = cursor.rowcount
             connection.commit()
-        if cursor.statusmessage.split()[1] == '0':
-            print(f"Could not find a student ID {student_id}\n")
+        
+        if rows_affected == 0:
+            print(f"Could not find a student with ID {student_id}\n")
         else:
-            print(f"Student ID {student_id} has been deleted\n")
+            print(f"Student with ID {student_id} has been successfully deleted\n")
 
     except psycopg2.Error as e:
         connection.rollback()
         print("\nError deleting the student: \n", e)
 
 def main():
-    
     database = input("\nEnter the database name: ")
-    username = input("Enter the databse username: ")
+    username = input("Enter the database username: ")
     password = input("Enter the database password: ")
 
-    with psycopg2.connect(dbname=database, user=username, password=password) as connection:
+    connect_to_database(database, username, password)
 
-        print("\nSelect one of the following options:\n")
-        print("    1. Print all of the students in the database")
-        print("    2. Add a new student into the databse")
-        print("    3. Update the email of a student in the databse")
-        print("    4. Delete a student in the databse")
-        print("    Press 0 to exit the program.\n")
+    print("\nSelect one of the following options:\n")
+    print("    1. Print all of the students in the database")
+    print("    2. Add a new student into the database")
+    print("    3. Update the email of a student in the database")
+    print("    4. Delete a student in the database")
+    print("    Press 0 to exit the program.\n")
 
-        with connection.cursor() as cursor:
-            while(not False):
-                result = input("Option: ")
-                result = result.upper()
+    while True:
+        result = input("Option: ").upper()
 
-                if(result == "0"):
-                    break
-                elif(result == "1"):
-                    getAllStudents(connection)
-                elif(result == "2"):
-                    first_name = input("\nEnter the first name of the student: ")
-                    last_name = input("Enter the last name of the student: ")
-                    email = input("Enter the email of the student: ")
-                    enrollment_date = input("Enter the enrollment date of the stuent (YYYY-MM-DD): ")
-                    addStudent(connection, first_name, last_name, email, enrollment_date)
-                elif(result == "3"):
-                    student_id = input("\nEnter the Student ID you want to update: ")
-                    new_email = input("Enter the new email of the student: ")
-                    updateStudentEmail(connection, student_id, new_email)
-                elif(result == "4"):
-                    student_id = input("\nEnter the ID of the student you want to delete: ")
-                    deleteStudent(connection, student_id)
-                else:
-                    print("Option entered is not valid.")
+        if result == "0":
+            break
+        elif result == "1":
+            getAllStudents()
+        elif result == "2":
+            first_name = input("\nEnter the first name of the student: ")
+            last_name = input("Enter the last name of the student: ")
+            email = input("Enter the email of the student: ")
+            enrollment_date = input("Enter the enrollment date of the student (YYYY-MM-DD): ")
+            addStudent(first_name, last_name, email, enrollment_date)
+        elif result == "3":
+            student_id = input("\nEnter the Student ID you want to update: ")
+            new_email = input("Enter the new email of the student: ")
+            updateStudentEmail(student_id, new_email)
+        elif result == "4":
+            student_id = input("\nEnter the ID of the student you want to delete: ")
+            deleteStudent(student_id)
+        else:
+            print("Option entered is not valid.")
 
-        print("\nThe database has disconnected.\n")
-        
+    print("\nThe database has disconnected.\n")
+    connection.close()
+
 # main function called
 if __name__ == "__main__":
     main()
