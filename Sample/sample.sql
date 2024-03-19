e060025a93b671215ae129677dad4569e74389a1
ZiHao257 <163956679+ZiHao257@users.noreply.github.com>
2024-03-19 14:17:31 -0400

    Update sample.sql

--- Sample/sample.sql
+++ Sample/sample.sql
@@ -1,1 +1,14 @@
+-- Database Schema for Table
+CREATE TABLE students (
+    student_id          SERIAL          PRIMARY KEY,
+    first_name          VARCHAR(255)    NOT NULL,
+    last_name           VARCHAR(255)    NOT NULL,
+    email               VARCHAR(255)    NOT NULL UNIQUE,
+    enrollment_date     DATE            DEFAULT CURRENT_DATE
+);
 
+-- Sample Data
+INSERT INTO students (first_name, last_name, email, enrollment_date) VALUES
+('John', 'Doe', 'john.doe@example.com', '2023-09-01'),
+('Jane', 'Smith', 'jane.smith@example.com', '2023-09-01'),
+('Jim', 'Beam', 'jim.beam@example.com', '2023-09-02');
